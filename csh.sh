1cd82a01c4fef8ce343571ba2d8744f0d1e3ee47
bruned12 <30997009+bruned12@users.noreply.github.com>
2019-03-09 20:10:07 +0800

    Update csh.sh

--- csh.sh
+++ csh.sh
@@ -2,4 +2,5 @@
 wget https://github.com/bruned12/BDSUbuntulib/blob/master/start.sh
 wget https://github.com/bruned12/BDSUbuntulib/tree/master/lib
 ln lib/libstdc++.so.6.0.22 lib/libstdc++.so.6
+chmod 755 start.sh
 rm -f csh.sh
